30bc26594c9842cdf92bfd75d1f215078fae19ec
Nikita <jennnikz12@gmail.com>
2024-08-07 18:22:09 +0530

    studentpage top layout done

--- frontend/src/landing_page/student/HeroStudent.js
+++ frontend/src/landing_page/student/HeroStudent.js
@@ -1,55 +1,44 @@
-{/* <div class="card" style="width: 18rem;">
-  <img src="..." class="card-img-top" alt="..."/>
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-  </div>
-  <ul class="list-group list-group-flush">
-    <li class="list-group-item">An item</li>
-    <li class="list-group-item">A second item</li>
-    <li class="list-group-item">A third item</li>
-  </ul>
-  <div class="card-body">
-    <a href="#" class="card-link">Card link</a>
-    <a href="#" class="card-link">Another link</a>
-  </div>
-</div> */}
+import React from 'react';
 
 function HeroStudent() {
     return ( 
-    <div class="row row-cols-1 row-cols-md-3 g-4">
-        <div class="col">
-            <div class="card">
-            <img src="..." class="card-img-top" alt="..."/>
-            <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-            </div>
+        <div className="justify-content-center p-5 text-center">
+            <div className="row">
+              <div className='col-1'></div>
+                <div className="col mb-4">
+                    <div className="card shadow" style={{width: "14rem"}}>
+                        <img src="../images/LMSlogo.jpg" className="card-img-top" style={{height: "50%", width: "100%"}} alt="..."/>
+                        <ul className="list-group list-group-flush">
+                            <li className="list-group-item"><a href="#" className='text-decoration-none text-reset'>Categories</a></li>
+                            <li className="list-group-item"><a href="#" className='text-decoration-none text-reset'>Rate Books</a></li>
+                            <li className="list-group-item"><a href="#" className='text-decoration-none text-reset'>Review Books</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div className="col mb-4">
+                    <div className="card shadow" style={{width: "14rem"}}>
+                        <img src="../images/LMSlogo.jpg" className="card-img-top" style={{height: "50%", width: "100%"}} alt="..."/>
+                        <ul className="list-group list-group-flush">
+                            <li className="list-group-item"><a href="#" className='text-decoration-none text-reset'>Issued</a></li>
+                            <li className="list-group-item"><a href="#" className='text-decoration-none text-reset'>Due Dates</a></li>
+                            <li className="list-group-item"><a href="#" className='text-decoration-none text-reset'>Fine</a></li>
+                            <li className="list-group-item"><a href="#" className='text-decoration-none text-reset'>Wishlist</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div className="col mb-4">
+                    <div className="card shadow" style={{width: "14rem"}}>
+                        <img src="../images/LMSlogo.jpg" className="card-img-top" style={{height: "50%", width: "100%"}} alt="..."/>
+                        <ul className="list-group list-group-flush">
+                            <li className="list-group-item"><a href="#" className='text-decoration-none text-reset'>Alerts</a></li>
+                            <li className="list-group-item"><a href="#" className='text-decoration-none text-reset'>Checkout</a></li>
+                            <li className="list-group-item"><a href="#" className='text-decoration-none text-reset'>Reserve</a></li>
+                        </ul>
+                    </div>
+                </div>
             </div>
         </div>
-        <div class="col">
-            <div class="card">
-            <img src="..." class="card-img-top" alt="..."/>
-            <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-            </div>
-            </div>
-        </div>
-        <div class="col">
-            <div class="card">
-            <img src="..." class="card-img-top" alt="..."/>
-            <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
-            </div>
-            </div>
-        </div>
-    </div>
-
     );
 }
 
 export default HeroStudent;
-
-
